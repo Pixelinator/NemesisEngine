7f9cf05baf2725f924bf370ca3e4f5f9370ad82e
Dominic Forster <forsterdominic2@gmail.com>
2023-08-12 16:51:53 +0200

    Added PBR Shaders

--- src/main/java/org/nemesis/Main.java
+++ src/main/java/org/nemesis/Main.java
@@ -4,8 +4,10 @@ import org.joml.Vector3f;
 import org.nemesis.entities.*;
 import org.nemesis.models.RawModel;
 import org.nemesis.models.TexturedModel;
-import org.nemesis.renderEngine.*;
-import org.nemesis.shaders.StaticShader;
+import org.nemesis.renderEngine.DisplayManager;
+import org.nemesis.renderEngine.Loader;
+import org.nemesis.renderEngine.MasterRenderer;
+import org.nemesis.renderEngine.OBJLoader;
 import org.nemesis.terrains.Terrain;
 import org.nemesis.textures.ModelTexture;
 
@@ -15,26 +17,27 @@ public class Main {
 
 	public static void main ( String[] args ) {
 		DisplayManager.createDisplay();
-		EntityManager entityManager = new EntityManager();
 		Loader loader = new Loader();
+		EntityManager entityManager = new EntityManager();
 		MasterRenderer masterRenderer = new MasterRenderer();
 
-		RawModel model = OBJLoader.loadObjModel( "dragon", loader );
 		ModelTexture texture = new ModelTexture( loader.loadTexture( "purple" ) );
+		ModelTexture grassTexture = new ModelTexture( loader.loadTexture( "grass" ) );
 		texture.setShineDamper( 10 );
 		texture.setReflectivity( 1 );
+		RawModel model = OBJLoader.loadObjModel( "dragon", loader );
 		TexturedModel texturedModel = new TexturedModel( model, texture );
 
 		Entity entity = entityManager.createEntity();
 		entity.addComponent( new TransformComponent( new Vector3f( 0, 0, -25 ), new Vector3f( 0, 0, 0 ), new Vector3f( 1, 1, 1 ) ) );
 		entity.addComponent( new MeshComponent( texturedModel ) );
 
-		Light light = new Light( new Vector3f(0,0,-20), new Vector3f(1,1,1) );
+		Light light = new Light( new Vector3f( 0, 0, -20 ), new Vector3f( 1, 1, 1 ) );
 
-		Terrain terrain1 = new Terrain( 0,0,loader, new ModelTexture( loader.loadTexture( "grass" ) ) );
-		Terrain terrain2 = new Terrain( -1,0,loader, new ModelTexture( loader.loadTexture( "grass" ) ) );
-		Terrain terrain3 = new Terrain( 0,-1,loader, new ModelTexture( loader.loadTexture( "grass" ) ) );
-		Terrain terrain4 = new Terrain( -1,-1,loader, new ModelTexture( loader.loadTexture( "grass" ) ) );
+		Terrain terrain1 = new Terrain( 0, 0, loader, grassTexture );
+		Terrain terrain2 = new Terrain( -1, 0, loader, grassTexture );
+		Terrain terrain3 = new Terrain( 0, -1, loader, grassTexture );
+		Terrain terrain4 = new Terrain( -1, -1, loader, grassTexture );
 
 		Camera camera = new Camera();
 
