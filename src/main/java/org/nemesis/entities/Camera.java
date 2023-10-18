0320f3d1fccaac04938f470130bfb12c718cec21
Dominic <forsterdominic2@gmail.com>
2023-10-18 16:12:55 +0200

    Added JBullet lib

--- src/main/java/org/nemesis/entities/Camera.java
+++ src/main/java/org/nemesis/entities/Camera.java
@@ -7,7 +7,7 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class Camera {
 
-	private Vector3f position = new Vector3f(0,0,0);
+	private Vector3f position = new Vector3f(0,5,0);
 	private float pitch;
 	private float yaw;
 	private float roll;
